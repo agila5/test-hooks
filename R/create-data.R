9ceef6cd21ea374318cc5fdda581f6c874b8eec7
Andrea Gilardi <andrea.gilardi@polimi.it>
2023-11-17 14:29:29 +0100

    update

--- R/create-data.R
+++ R/create-data.R
@@ -2,5 +2,5 @@ dd <- do.call(rbind, replicate(20, iris, simplify = FALSE))
 write.csv2(dd, "data/iris_small.csv")
 
 
-dd <- do.call(rbind, replicate(1e3, iris, simplify = FALSE))
+dd <- do.call(rbind, replicate(3e4, iris, simplify = FALSE))
 write.csv2(dd, "data/iris_large.csv")
